ea4e1fa8e92235b7fe441eb87ef08a3602450307
Julio Enrique Rodriguez Angulo <julio.rodriguez1@yahoo.com>
2021-05-02 17:10:06 -0300

    commit primero

---
+++ program.js
@@ -0,0 +1,22 @@
+//Autor: curso Programacion 2021
+//Ubicacion: Buenos Aires
+
+/*************************
+DECLARACIÓN DE VARIABLES *
+Y CONSTANTES             *
+/*************************/
+let nombre = "Pedro";
+let edad = 37;
+let _genero = "masculino";
+let $apellido = "Ramirez";
+let sueldo: 800000;
+
+console.log(nombre, $apellido, edad, _genero, "sueldo: "+sueldo);
+
+let nombre = "Julio";
+let edad = 35;
+let _genero = "masculino";
+let $apellido = "Rodriguez";
+let sueldo = 80000;
+
+console.log(nombre, $apellido, edad, _genero, "sueldo: "+sueldo);
